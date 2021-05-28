9029d467909442df36d78882f533f8ae851073b3
murphbra <murphbra@oregonstate.edu>
2021-05-27 20:13:03 -0600

    update

--- project.js
+++ project.js
@@ -1,33 +1,37 @@
-var slideIndex = 0;
-showSlides();
+<!-- this code is adapted from https://www.w3schools.com/howto/howto_js_slideshow.asp -->
 
-function showSlides() {
+var pageIndex = 0;
+carousel();
+
+function carousel() {
+
   var i;
   var slides = document.getElementsByClassName("pageSlides");
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
-  slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}
-  slides[slideIndex-1].style.display = "block";
-  setTimeout(showSlides, 2000); // Change image every 2 seconds
+  pageIndex++;
+  if (pageIndex > slides.length) {pageIndex = 1}
+  slides[pageIndex-1].style.display = "block";
+  setTimeout(carousel, 2000); 
 }
 function navSlides(n) {
-  chooseSlides(slideIndex += n);
+    <!-- calls chooseSlides function and passes it current pageIndex +/- 1 when previous or next button is pressed on home page -->
+  chooseSlides(pageIndex += n);
 }
 
 function chooseSlides(n) {
   var i;
   var slides = document.getElementsByClassName("pageSlides");
-  var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
+  //var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {pageIndex = 1}
+  if (n < 1) {pageIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
       slides[i].style.display = "none";
   }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
+  //for (i = 0; i < dots.length; i++) {
+      //dots[i].className = dots[i].className.replace(" active", "");
+  //}
+  slides[pageIndex-1].style.display = "block";
+  //dots[pageIndex-1].className += " active";
 }
